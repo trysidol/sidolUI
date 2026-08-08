98f2c16a21c546ada4772397a28c488577e2b295
mitayan0 <mitayanchangma@gmail.com>
2026-08-08 10:41:55 +0600

    feat: native dev launcher and opt-in HTML preview

--- sidol/__init__.py
+++ sidol/__init__.py
@@ -2,24 +2,48 @@
 
 from sidol.app import App
 from sidol.component import Component, State, reset_graph
+from sidol.events import FocusEvent, KeyEvent, normalise_key
 from sidol.node import Node
+from sidol.surfaces.html import export_html
+from sidol.surfaces.tui import TuiSurface
 from sidol.theme import Style, Theme, get_theme, set_theme
-from sidol.widgets import Button, Column, Row, Spacer, Text
+from sidol.widgets import (
+    Button,
+    Column,
+    Dropdown,
+    List,
+    Row,
+    ScrollView,
+    Slider,
+    Spacer,
+    Text,
+    TextField,
+)
 
 __all__ = [
     "App",
     "Component",
     "State",
     "reset_graph",
+    "FocusEvent",
+    "KeyEvent",
+    "normalise_key",
     "Node",
     "Style",
     "Theme",
     "get_theme",
     "set_theme",
+    "TuiSurface",
+    "export_html",
     "Button",
     "Column",
+    "Dropdown",
+    "List",
     "Row",
+    "ScrollView",
+    "Slider",
     "Spacer",
     "Text",
+    "TextField",
 ]
 __version__ = "0.1.0"
